6950e676a262164f4acfbd2f1e72b3f8c1944ff5
Michal Stankevič <michalito.stankiewicz@gmail.com>
2018-05-28 12:59:06 +0200

    Structure MVC architecture. Add robot model and robot controller. Not all methods implemented.

---
+++ Autoukis/Autoukis/Controllers/RobotController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Autoukis.Models;
+
+namespace Autoukis.Controllers
+{
+    public class RobotController
+    {
+        public List<Robot> Robots { get; }
+
+        public (Position position, List<byte[]> view) CheckState(int robotId)
+        {
+            var robot = Robots.Find(x => x.Id == robotId);
+            return (robot.Position, robot.View);
+        }
+
+        public List<Robot> GetAvailableRobots()
+        {
+            return Robots.Where(x => x.Availability).ToList();
+        }
+
+        public void SendSignal(int robotId)
+        {
+            
+        }
+    }
+}
---
+++ Autoukis/Autoukis/Models/Robot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Autoukis.Models
+{
+    public class Robot: TechnicalEquipment
+    {
+        public double BatteryLevel { get; set; }
+        public bool Availability { get; set; }
+        public List<byte[]> View { get; set; }
+
+        public void GoTowardsBrokenEquipment(Position position)
+        {
+            
+        }
+
+        public void Demount(int equipmentId)
+        {
+            
+        }
+
+        public void DragToWarehouse(int equipmentId)
+        {
+            
+        }
+
+        public void Alive()
+        {
+            
+        }
+    }
+}
